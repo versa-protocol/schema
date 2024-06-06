06b24efde4716e09e0adf194e4efa52d1849aee8
Thomas Constantine Moore <tomismore@gmail.com>
2024-06-06 16:30:18 -0400

    ditch quicktype for json2ts

--- build_ts.sh
+++ build_ts.sh
@@ -1,5 +1,2 @@
-quicktype -o web/schema.ts --src-lang schema data_objects/*
-
-# json2ts -i data_objects/ -o web/schema.ts
-
+pnpm exec json2ts data/receipt.schema.json > web/schema.ts
 git add web/schema.ts
