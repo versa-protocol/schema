fcf8be4f4acf788e33d395d3317f94011622709a
Thomas Constantine Moore <tomismore@gmail.com>
2024-10-17 16:56:48 -0400

    1.5 (#2)
    * WIP to appropriately title definitions, add envelope
    
    * drop deprecated build scripts and rust toolchain
    
    * only mark truly required fields as required in header
    
    * add more titles to types within the receipt schema
    
    * more definition titles and correcting lists of required fields
    
    * correct definition placement of customer
    
    * format
    
    * name SchemaVersion and title SubscriptonItem
    
    * title a Shipment def and review required fields on itemizations

--- build_rs.sh
+++
@@ -1,6 +0,0 @@
-quicktype -o src/receipt.rs --src-lang schema data/receipt.schema.json \
-          --density dense \
-          --visibility public \
-          --derive-debug
-cargo fmt --
-git add src/receipt.rs
--- build_ts.sh
+++
@@ -1,1 +0,0 @@
-pnpm exec json2ts data/receipt.schema.json > web/schema.ts --ignoreMinAndMaxItems --additionalProperties false
