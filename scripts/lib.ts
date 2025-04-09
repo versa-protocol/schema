e0adffb16943796ea74beeb1b50ebc251877e824
Thomas Moore <thomas@versa.org>
2025-04-08 22:34:00 -0400

    add metavalidation script

---
+++ scripts/lib.ts
@@ -0,0 +1,23 @@
+export const isObjectSubset = (
+  subObject: object,
+  superObject: object,
+  strict = false,
+): boolean => {
+  for (const key of Object.keys(subObject)) {
+    if (key == "additionalProperties") {
+      continue;
+    }
+    if (!Object.hasOwn(superObject, key)) {
+      console.log("missing", key);
+      return false;
+    }
+    const subValue = (subObject as any)[key];
+    const superValue = (superObject as any)[key];
+    if (typeof subValue === "object" && typeof superValue === "object") {
+      if (!isObjectSubset(subValue, superValue, strict)) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
