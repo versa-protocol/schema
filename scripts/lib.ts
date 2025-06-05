474b186adab5874632fe01a56f89eaacc39d2bbb
Thomas Moore <thomas@versa.org>
2025-06-05 15:07:55 -0400

    update guests, add the _at suffix to current periods, add regex validation/mins/maxes

--- scripts/lib.ts
+++ scripts/lib.ts
@@ -1,6 +1,7 @@
 export const isObjectSubset = (
   subObject: object,
   superObject: object,
+  parentKey: string = "root",
   strict = false,
 ): boolean => {
   for (const key of Object.keys(subObject)) {
@@ -11,19 +12,28 @@ export const isObjectSubset = (
     const subValue = (subObject as any)[key];
     const superValue = (superObject as any)[key];
     if (typeof subValue === "object" && typeof superValue === "object") {
-      if (!isObjectSubset(subValue, superValue, strict)) {
+      if (!isObjectSubset(subValue, superValue, key, strict)) {
         return false;
       }
     }
-    if (key === "pattern" && !!subValue) {
-      if (subValue !== superValue) {
-        console.log("pattern mismatch", superValue, subValue);
+    // The following two clauses don't really belong here;
+    // generally this verification process should be improved to ensure
+    // all formatting patterns, mins, and maxes are uniform between schemas
+    if (key === "pattern") {
+      if (!!superValue && !subValue) {
+        console.log("pattern must always be defined for", parentKey);
+      }
+      if (!!subValue && subValue !== superValue) {
+        console.log("pattern mismatch for", parentKey, superValue, subValue);
         return false;
       }
     }
-    if (key === "type" && !!subValue) {
+    if (key === "type") {
+      if (!!superValue && !subValue) {
+        console.log("type must always be defined for", parentKey);
+      }
       if (subValue.toString() !== superValue.toString()) {
-        console.log("type mismatch", superValue, subValue);
+        console.log("type mismatch for", parentKey, superValue, subValue);
         return false;
       }
     }
